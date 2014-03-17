af4da58c3093cf48a13a8d9472691b9b6456940b
Ivan Kar <i.kar.kh@gmail.com>
2014-03-17 12:03:02 -0400

    removed js from html to .js file and formatted it

--- js/up_down_catalog.js
+++ js/up_down_catalog.js
@@ -1,26 +1,26 @@
-	
-	function verticalMove(check,increment){
-		var table = document.getElementById("catalog");
-		var rows = table.getElementsByTagName("tr");
-		for (var i = 0; i < rows.length; i++) {
-			if (rows[i].className === "selectedRow") {				
-				if (check(i,rows)) {				
-					rows[i + increment].className = "selectedRow";	
-					rows[i].className = "";
-				}
-				break;
-			}
-		}
+	function verticalMove(check, increment) {
+	    var table = document.getElementById("catalog");
+	    var rows = table.getElementsByTagName("tr");
+	    for (var i = 0; i < rows.length; i++) {
+	        if (rows[i].className === "selectedRow") {
+	            if (check(i, rows)) {
+	                rows[i + increment].className = "selectedRow";
+	                rows[i].className = "";
+	            }
+	            break;
+	        }
+	    }
 	}
+
 	function up() {
-		verticalMove(function(i,rows){
-			return i - 1 >= 0;
-		}, -1);
+	    verticalMove(function(i, rows) {
+	        return i - 1 >= 0;
+	    }, -1);
 	}
 
 	function down() {
-		
-		verticalMove(function(i,rows){
-			return i + 1 < rows.length;
-		}, 1);	
+
+	    verticalMove(function(i, rows) {
+	        return i + 1 < rows.length;
+	    }, 1);
 	}
