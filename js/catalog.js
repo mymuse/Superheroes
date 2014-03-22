1ea06e8940458177bb4dcc3d9a51d51f1e89d187
Alena Chuchalina <alena.chuchalina@gmail.com>
2014-03-22 07:18:23 -0400

    lab 6(jquery)

--- js/catalog.js
+++ js/catalog.js
@@ -1,41 +1,35 @@
 function initFrimSelectors() {
-    $(".firm > .firm_header").click(function(){
-		var info = $(this).siblings(".firm_info");
-		if(info.hasClass("firm_info_hidden")){
-			info.removeClass("firm_info_hidden");
-			info.addClass("firm_info_visible");
-		}else{
-			info.removeClass("firm_info_visible");
-			info.addClass("firm_info_hidden");
-		}
+    $(".firm > .firm_header").click(function() {
+        var info = $(this).siblings(".firm_info");
+        if (info.hasClass("firm_info_hidden")) {
+            info.removeClass("firm_info_hidden");
+            info.addClass("firm_info_visible");
+        } else {
+            info.removeClass("firm_info_visible");
+            info.addClass("firm_info_hidden");
+        }
     });
 }
 
 function initial() {
-    $("#catalog > tbody > tr").click(function(){
-    	$(".selectedRow").removeClass("selectedRow");
-    	$(this).addClass("selectedRow");
+    $("#catalog > tbody > tr").click(function() {
+        $(".selectedRow").removeClass("selectedRow");
+        $(this).addClass("selectedRow");
     });
     initFrimSelectors();
 }
 
 function bet() {
-	var bet_dom = $(".selectedRow").find(".bet");
+    var bet_dom = $(".selectedRow").find(".bet");
     bet_dom[0].checked = !bet_dom[0].checked;
 }
 
 //do it, i think its complex and include all fetures we should know in jquery
 function send() {
-    var table = document.getElementById("catalog");
-    var rows = table.getElementsByTagName("tr");
-
+    var rows = $("#catalog > tbody > tr:has(.bet:checked)");
     var win = window.open("", "", "");
-
-    for (var i = 0; i < rows.length; i++) {
-        var bet_i = rows[i].getElementsByClassName("bet")[0];
-        if (bet_i.checked) {
-            var h1s = rows[i].getElementsByClassName("h1_catalog");
-            win.document.writeln("Name " + h1s[0].innerHTML + ", price - " + h1s[1].innerHTML + "<br>");
-        }
+    for (var row in rows) {
+        var h1s = row.getElementsByClassName("h1_catalog");
+        win.document.writeln("Name " + h1s[0].innerHTML + ", price - " + h1s[1].innerHTML + "<br>");
     }
 }
